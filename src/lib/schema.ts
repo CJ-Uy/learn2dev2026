61e914d688030ddad5235f110929a777908a18af
bea-ndc <beatrice.decastro@student.ateneo.edu>
2026-04-15 21:27:04 +0800

    Added events table in schema
    Added table for events with necessary details

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -4,12 +4,24 @@ import { sql } from "drizzle-orm";
 // Add your tables here.
 // Example starter table — replace or extend as needed.
 
-export const example = sqliteTable("example", {
+// export const example = sqliteTable("example", {
+//   id: text("id")
+//     .primaryKey()
+//     .$defaultFn(() => crypto.randomUUID()),
+//   name: text("name").notNull(),
+//   createdAt: text("created_at")
+//     .notNull()
+//     .default(sql`(datetime('now'))`),
+// });
+
+export const event = sqliteTable("events",{
   id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
-  name: text("name").notNull(),
-  createdAt: text("created_at")
+  eventTitle: text("event_title").notNull(),
+  event_date: integer("event_date", { mode: "timestamp" })
     .notNull()
-    .default(sql`(datetime('now'))`),
+    .default(sql`(datetime('now', '+8 hours'))`),
+  event_desc: text("event_desc"),
+  event_dur: integer("event_dur").notNull().default(30),
 });
