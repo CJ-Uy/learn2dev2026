830c7eb1f5acb3f7543c30959f186a38bed117f8
mcfc <gregoriodelfinpascua@gmail.com>
2026-04-18 22:34:09 +0800

    feat: basic sign up page

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -3,6 +3,7 @@
 import { authClient } from "@/lib/auth-client";
 import { useActionState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function SignUpPage() {
     const router = useRouter();
@@ -26,13 +27,52 @@ export default function SignUpPage() {
     const [error, action] = useActionState(signUp, "");
 
     return (
-        <form action={action}>
-            <input name="name" type="text" placeholder="Name" required/>
-            <input name="email" type="email" placeholder="Email" required/>
-            <input name="password" type="password" placeholder="Password" required/>
-            {error && <p>{error}</p>}
-            <button type="submit">Sign Up</button>  
-        </form>
-    );
+        <main className="min-h-screen flex items-center justify-center bg-[#c7d1f0]">
+            <section className="flex w-full max-w-4xl overflow-hidden rounded-2xl shadow-lg">
+                <div className="w-full max-w-md bg-[#3758BF] px-6 py-10">
+                    <h1 className="text-3xl font-semibold text-[#FFFFFF] text-left p-4"> Create your account</h1>
 
+                    <form action={action} className="mt-8 p-4 flex flex-col gap-4">
+
+                        <div className="space-y-2 shadow-lg">
+                            <label htmlFor="name" className="font-semibold">Name</label>
+                            <input id="name" name="name" type="text" placeholder="Name" required
+                            className="w-full rounded-2xl bg-[#F8EACD] px-4 py-3 text-amber-950"/>
+                        </div>
+
+                        <div className="space-y-2 shadow-lg">
+                            <label htmlFor="email" className="font-semibold">Email</label>
+                            <input id="email" name="email" type="email" placeholder="Email" required
+                            className="w-full rounded-2xl bg-[#F8EACD] px-4 py-3 text-amber-950"/>
+                        </div>
+
+                        <div className="space-y-2 shadow-lg">
+                            <label htmlFor="password" className="font-semibold">Password</label>
+                            <input id="password" name="password" type="password" placeholder="Password" required
+                            className="w-full rounded-2xl bg-[#F8EACD] px-4 py-3 text-amber-950"/>
+                        </div>
+
+                        <div className="space-y-2 text-center">
+                            <button type="submit"
+                            className="w-full rounded-2xl bg-[#F8DE59] py-3 text-black font-extrabold ring-5 ring-white ring-inset
+                            transition duration-200 hover:translate-y-0.5 hover:bg-[#f2d53a] hover:shadow-lg ">
+                                Sign Up
+                            </button>  
+                        </div>
+                    </form>
+
+                    <p className="px-4 font-semibold"> Already have an account?{" "}
+                        <a href="/login" className="text-[#F8DE59] font-semibold">
+                            Login
+                        </a>
+                    </p>
+                </div>
+                
+                <div className="relative hidden md:block flex-1">
+                    <Image src="/gonzaga.jpg" alt="Gonzaga Cafeteria" fill sizes="(max-width: 768px) 0px, 420px" className="object-cover"/>
+                </div>
+
+            </section>
+        </main>
+    );
 }
